23f40028bc678a2d82b33f5b3638b6c2f6e6348f
Ritvik <ritvik.gupta2205@gmail.com>
2025-01-19 01:48:04 +0000

    splashers dont attack walls

--- src/Iteration4/Robot/Splasher.java
+++ src/Iteration4/Robot/Splasher.java
@@ -169,7 +169,7 @@ public class Splasher extends Robot {
         MapInfo[] surrMapInfos = rc.senseNearbyMapInfos(4);
         int emptyTiles = 0;
         for (MapInfo mapInfo : surrMapInfos) {
-            if (!mapInfo.getPaint().isAlly()) {
+            if (!mapInfo.getPaint().isAlly() && mapInfo.isPassable() && !mapInfo.isWall()) {
                 rc.setIndicatorDot(mapInfo.getMapLocation(), 255, 255, 0);
                 emptyTiles++;
             }
